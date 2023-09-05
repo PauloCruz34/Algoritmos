b94e7844542ff09326e52b73a03f5b6a2e230096
Paulo Afonso da Cruz <126684518+PauloCruz34@users.noreply.github.com>
2023-09-05 20:53:42 -0300

    Add files via upload

--- exerciciosLista_01/exercicios.py
+++ exerciciosLista_01/exercicios.py
@@ -1,12 +1,29 @@
+#Exercício 29
 print ("\nÉ PRECISO FAZER TODOS OS ALGORITMOS PARA APRENDER")
 print ("\n")
 
+#Exercício 30
 print("Paulo Afonso da Cruz")
 print("\n")
 
+#Exercício 31
 produto = 28 * 43
 print(f"O produto entre os dois é {produto}")
 print('\n')
 
+#Exercício 32
 ma = (8 + 9 + 7)/3
 print(f'A média aritmetica entre os tres é de {ma}')
+print("\n")
+
+#Exercício 33
+num = int(input("Insira m número inteiro: "))
+print(num)
+print("\n")
+
+#Exercício 34
+num1 = int(input("insira um número inteiro: "))
+num2 = int(input("insira outro número inteiro: "))
+print(num1)
+print(num2)
+print("\n")
