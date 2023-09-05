9bff78859307305c4aef7581d714889fd64c4622
Paulo Afonso da Cruz <126684518+PauloCruz34@users.noreply.github.com>
2023-09-04 22:10:29 -0300

    primeira aula variáveis python

---
+++ aula_variaveis.py
@@ -0,0 +1,14 @@
+var_int =  13 #número inteiro
+var_float = 45.6 # numero decimal
+var_bool = True  # verdadeiro ou falso
+var_str = "Bem vindo" # str não pode esquecer as aspas
+var_int =  13.8 #número inteiro
+
+print(type(var_int))
+print(type(var_float))   # type volta o tipo do dado
+print(type(var_bool))    # print sempre em parênteses 
+print(type(var_str))
+
+
+
+
