cc2e2159c7967800c9492a970baf03df4ab5b485
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2023-09-18 21:11:05 -0300

    exercicio

---
+++ exercicios_lista2/exercicio_159.py
@@ -0,0 +1,8 @@
+import math as m
+x = float(input("\n Digite o valor de x: "))
+if x > 4 or x < -4 :
+    fx = (5 * x + 3)/ m.sqrt(x**2 - 16)    
+    print(f"\n f(x)= {fx}")
+else:
+    print("\n Não pode ser feita")
+    
