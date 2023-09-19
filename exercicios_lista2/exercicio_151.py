e9ab5b3cb5bad4746b8a7c0c9b85a4df1ae7f26c
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2023-09-18 21:58:15 -0300

    exercicio

---
+++ exercicios_lista2/exercicio_151.py
@@ -0,0 +1,13 @@
+peso = float(input("\n Digite seu peso:"))
+alt = float(input("\n Digite sua alt: "))
+imc = peso / alt**2
+if imc < 20:
+    print("\n Abaixo do peso.")
+elif imc <= 25:
+    print("\n Normal.")
+elif imc <= 30:
+    print("\n Sobrepeso.")
+elif imc <= 35:
+    print("\n Obeso.")
+else:
+    print("\n Obesidade mórbida.")
