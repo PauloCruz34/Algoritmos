d914540d8f4b13674f0259f6e95142501268e7d4
Paulo Afonso da Cruz <paulo.cruz20@fatec.sp.gov.br>
2023-09-25 22:08:36 -0300

    exercicio de fixação

--- repeticoes/exRepete.py
+++ repeticoes/exRepete.py
@@ -9,3 +9,12 @@ for tabuada in range(1,11):
     print("\nTabuada do ", tabuada)
     for i in range(1,11):
         print(tabuada, " x ", i, " = ", tabuada * i)
+        
+lista = [23,35,36,78,98]
+for x in lista:
+    print(x)
+    
+dict_elementos = {"a":23, "b": 56}
+for elementos in dict_elementos:
+    print (elementos)
+    
